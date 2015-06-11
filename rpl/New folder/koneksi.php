8b6f0e240944fac17cc8fcdf713929bcc17a25cf
Tarmizi Ramud <ramudtarmizi@gmail.com>
2015-06-11 17:10:07 +0700

    sebuah folder dihapus karena tidak perlu dalam tugas

--- rpl/New folder/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host="localhost";
-	$user="root";
-	$password="";
-	$database="warkop2";
-
-	mysql_connect($host,$user,$password) or die ("Gagal Terhubung Ke Database");
-	mysql_select_db($database) or die ("Database Tidak Ditemukan");
-?>
